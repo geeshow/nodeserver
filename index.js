df51d526921398871fe936bf12ef89f90e6482fa
geeshow <kute80@naver.com>
2020-07-08 23:26:47 +0900

    impl login by jsonwebtoken and bcrypt

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const config = require('./config/key')
 const { User } = require("./models/User")
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -28,4 +30,28 @@ app.post('/register', (req, res) => {
   })
 })
 
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email}, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSucess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+      
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSucess:true, userId: user._id })
+
+      })
+    })
+  })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
